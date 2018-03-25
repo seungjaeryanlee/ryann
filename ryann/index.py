d9d1e45d27331d9338933b4a0f20c8e7982993d6
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-03-25 16:07:15 +0900

    Setup pylint (#4)
    * Fixed or disabled all pylint errors
    
    * Added Pylint to TravisCI script
    
    * Added pylint to TravisCI install dependencies

--- ryann/index.py
+++ ryann/index.py
@@ -1,2 +1,8 @@
+"""
+A sample module for pytest.
+"""
 def hello_world():
+    """
+    Outputs 'Hello World'.
+    """
     return 'Hello World'
--- ryann/tests/test_index.py
+++ ryann/tests/test_index.py
@@ -1,6 +1,16 @@
+# pylint: disable=no-self-use, too-few-public-methods
+"""
+A sample test file for a module.
+"""
 from ryann import index
 
 
 class TestIndex:
+    """
+    Tests all functions in ryann.index.
+    """
     def test_hello_world(self):
+        """
+        Tests the output of ryann.index.hello_world().
+        """
         assert index.hello_world() == 'Hello World'
