4f056537e2e3e082c473dc70b096ad782a079579
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-03-27 15:36:49 +0900

    Added docstring for nn.sigmoid()

---
+++ ryann/nn.py
@@ -0,0 +1,20 @@
+"""
+Defines a basic neural network model.
+"""
+
+def sigmoid(x):
+    """
+    Apples the sigmoid function to a number or to a NumPy ndarray.
+
+    Parameters
+    ----------
+    x : float or np.ndarray
+        A single number or a ndarray to apply the sigmoid function.
+
+    Returns
+    -------
+    float or ndarray
+        If x was a single float, returns sigmoid(x). If x was an np.ndarray, returns a np.ndarray with sigmoid function
+        applied to each element of the ndarray.
+    """
+    pass
