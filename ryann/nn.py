bf1ad115699ce8c7b4a811aa9bd3dc758d1caf25
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-03-27 17:40:44 +0900

    Fixed bug where last activation function was not sigmoid

--- ryann/nn.py
+++ ryann/nn.py
@@ -127,7 +127,7 @@ def _forward_propagation(X, parameters):
     Z1 = np.dot(parameters['W1'], X) + parameters['b1']
     A1 = np.tanh(Z1)
     Z2 = np.dot(parameters['W2'], A1) + parameters['b2']
-    A2 = np.tanh(Z2)
+    A2 = sigmoid(Z2)
 
     cache = {
         'Z1': Z1,
