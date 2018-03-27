33ded6b0a0f69a0194424f1b8ae442a652f60ad6
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-03-27 16:15:07 +0900

    Implemented nn.sigmoid() (#6)
    * Added empty nn.py and test_nn.py
    
    * Added docstring for nn.sigmoid()
    
    * Added tests for nn.sigmoid()
    
    * Implemented nn.sigmoid()
    
    * Fixed pylint errors
    
    * Autogenerated Sphinx docs
    
    * Fixed typo in nn.sigmoid() docstring

---
+++ ryann/nn.py
@@ -0,0 +1,23 @@
+# pylint: disable=invalid-name
+"""
+Defines a basic neural network model.
+"""
+import numpy as np
+
+
+def sigmoid(x):
+    """
+    Applies the sigmoid function to a number or to a NumPy ndarray.
+
+    Parameters
+    ----------
+    x : float or np.ndarray
+        A single number or a ndarray to apply the sigmoid function.
+
+    Returns
+    -------
+    float or ndarray
+        If x was a single float, returns sigmoid(x). If x was an np.ndarray, returns a np.ndarray
+        with sigmoid function applied to each element of the ndarray.
+    """
+    return 1 / (1 + np.exp(-x))
