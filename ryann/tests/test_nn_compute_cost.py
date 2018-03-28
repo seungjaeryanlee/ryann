aa82f7190e1f636100bdec04d8c5dea2f85c7fe2
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-03-28 12:47:01 +0900

    Added partial tests for nn._compute_cost()

---
+++ ryann/tests/test_nn_compute_cost.py
@@ -0,0 +1,43 @@
+# pylint: disable=no-self-use, too-few-public-methods, invalid-name
+"""
+Tests the function nn._compute_cost().
+"""
+import numpy as np
+from numpy.testing import assert_almost_equal
+from ryann import nn
+
+
+def test_nn_compute_cost_close():
+    """
+    Check that nn._compute_cost() returns close to zero if Y_computed is close to Y.
+    """
+    m = 10
+    epsilon = 10**-8
+
+    Y = np.random.randint(0, 2, size=(1, m))
+    Y_computed = np.abs(Y - epsilon)
+    cost = nn._compute_cost(Y_computed, Y)
+
+    print('Y : ' + str(Y))
+    print('Y_computed : ' + str(Y_computed))
+    print('Cost : ' + str(cost))
+
+    assert_almost_equal(cost, 0)
+
+
+def test_nn_compute_cost_half():
+    """
+    Check that nn._compute_cost() returns - log(0.5) if Y_computed is all 0.5.
+    """
+    m = 10
+    delta = 10**-1
+
+    Y = np.random.randint(0, 2, size=(1, m))
+    Y_computed = np.full((1, m), 0.5)
+    cost = nn._compute_cost(Y_computed, Y)
+
+    print('Y : ' + str(Y))
+    print('Y_computed : ' + str(Y_computed))
+    print('Cost : ' + str(cost))
+
+    assert_almost_equal(cost, -np.log(0.5))
