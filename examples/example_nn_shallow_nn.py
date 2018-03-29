104c1693f5bcbca951c00eeea3b6c551ad55573c
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-03-29 16:33:36 +0900

    Added decision boundary plot to nn.shallow_nn() example

--- examples/example_nn_shallow_nn.py
+++ examples/example_nn_shallow_nn.py
@@ -20,14 +20,22 @@ for i, x in enumerate(X.T):
 # Y = [ int(x[0] + x[1] < 1 + np.random.randn() * noise) for x in X.T]
 
 # 1) Plot Data
-plt.scatter(X[0, :], X[1, :], c=Y.reshape(m), cmap='coolwarm')
-plt.show(block=False)
-plt.pause(0.01)
+# plt.scatter(X[0, :], X[1, :], c=Y.reshape(m), cmap='coolwarm')
 
 # 2) Train
 parameters, costs = nn.shallow_nn(X, Y, n_h=3, num_iter=10000)
 
 # 3) Compute and Plot Decision Boundary
+x1_pts = np.arange(-5, 5, 0.01)
+x2_pts = np.arange(-5, 5, 0.011)
+x1_grid, x2_grid = np.meshgrid(x1_pts, x2_pts)
+X_grid = np.concatenate((x1_grid.reshape(1, -1), x2_grid.reshape(1, -1)))
+Z = nn.predict(parameters, X_grid)
+Z = Z.reshape((len(x1_pts), len(x2_pts)))
+
+plt.contourf(x1_grid, x2_grid, Z, cmap='coolwarm')
+plt.scatter(X[0, :], X[1, :], c=Y.reshape(m), cmap='coolwarm')
+
 
 # 4) Finish
 plt.show() # Continue showing plot after script finishes
