a23574d7e611cc413e78bab3cbfacea8896bc827
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-03-30 15:09:37 +0900

    Renamed and updated example_nn_shallow.py

--- examples/example_nn_shallow.py
+++ examples/example_nn_shallow.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This file showcases what ryann.nn.train() does.
+This file showcases the ryann.nn.shallow module.
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-from ryann import nn
+from ryann.nn import shallow
 
 
 # 0) Synthesize Data
@@ -21,14 +21,14 @@ for i, x in enumerate(X.T):
 # plt.scatter(X[0, :], X[1, :], c=Y.reshape(m), cmap='coolwarm')
 
 # 2) Train
-parameters, costs = nn.train(X, Y, n_h=3, num_iter=10000)
+parameters, costs = shallow.train(X, Y, n_h=3, num_iter=10000)
 
 # 3) Compute and Plot Decision Boundary
 x1_pts = np.arange(-5, 5, 0.01)
 x2_pts = np.arange(-5, 5, 0.01)
 x1_grid, x2_grid = np.meshgrid(x1_pts, x2_pts)
 X_grid = np.concatenate((x1_grid.reshape(1, -1), x2_grid.reshape(1, -1)))
-Z = nn.predict(parameters, X_grid)
+Z = shallow.predict(parameters, X_grid)
 Z = Z.reshape((len(x1_pts), len(x2_pts)))
 
 plt.contourf(x1_grid, x2_grid, Z, cmap='coolwarm')
