f27c83a26b59cae36b871d92dd416bc92f28ec7c
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-04-02 14:39:50 +0900

    Copied example from shallow to deep

---
+++ examples/example_nn_deep.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+This file showcases the ryann.nn.deep module.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+from ryann.nn import deep
+
+
+# 0) Synthesize Data
+m = 200
+X = np.random.randn(2, m)
+Y = np.zeros(shape=(1, m))
+noise = 1
+for i, x in enumerate(X.T):
+    Y[0][i] = int(x[0]**2 + 2 * x[1] < 1 + noise * np.random.choice([0, 1]))
+# plt.scatter(X[0, :], X[1, :], c=Y.reshape(m), cmap='coolwarm')
+
+# 2) Train
+parameters, costs = deep.train(X, Y, layer_dims=[2, 2, 2, 1], num_iter=10000)
+
+# 3) Compute and Plot Decision Boundary
+x1_pts = np.arange(-5, 5, 0.01)
+x2_pts = np.arange(-5, 5, 0.01)
+x1_grid, x2_grid = np.meshgrid(x1_pts, x2_pts)
+X_grid = np.concatenate((x1_grid.reshape(1, -1), x2_grid.reshape(1, -1)))
+Z = deep.predict(parameters, X_grid)
+Z = Z.reshape((len(x1_pts), len(x2_pts)))
+
+plt.contourf(x1_grid, x2_grid, Z, cmap='coolwarm')
+plt.scatter(X[0, :], X[1, :], c=Y.reshape(m), cmap='coolwarm')
+plt.show()
