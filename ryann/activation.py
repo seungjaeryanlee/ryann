a31315d6b947f8eaa3fbba15e0cb031d23a3b825
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-04-02 14:08:58 +0900

    Added relu() and relu_derivative() to activation.py

--- ryann/activation.py
+++ ryann/activation.py
@@ -41,3 +41,39 @@ def sigmoid_derivative(x):
 
     """
     return sigmoid(x) * (1 - sigmoid(x))
+
+def relu(x):
+    """
+    Applies the ReLU function to a number or to a NumPy ndarray.
+
+    Parameters
+    ----------
+    x : float or np.ndarray
+        A single number or a ndarray to apply the ReLU function.
+
+    Returns
+    -------
+    float or ndarray
+        If x was a single float, returns ReLU(x). If x was an np.ndarray, returns a np.ndarray
+        with ReLU function applied to each element of the ndarray.
+    """
+    return np.maximum(x, 0)
+
+
+def relu_derivative(x):
+    """
+    Applies the derivative of the ReLU function to a number or to a NumPy ndarray. When x = 0,
+    it returns 0.
+
+    Parameters
+    ----------
+    x : float or np.ndarray
+        A single number or a ndarray to apply the derivative of the ReLU function.
+
+    Returns
+    -------
+    float or ndarray
+        If x was a single float, returns ReLU(x). If x was an np.ndarray, returns a np.ndarray
+        with the derivative of the ReLU applied to each element of the ndarray.
+    """
+    return (x > 0) * 1
