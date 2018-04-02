2b080ba1dea63a599329acb21f4ac9d76c030932
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-04-02 13:21:27 +0900

    Modified tests for _initialize_parameters() for new parameters format

--- ryann/tests/nn/deep/test_initialize_parameters.py
+++ ryann/tests/nn/deep/test_initialize_parameters.py
@@ -7,34 +7,33 @@ from ryann.nn import deep
 
 def test_nn_deep_initialize_parameters_dimensions():
     """
-    Checks the dimensions of output parameters of nn.shallow._initialize_parameters().
+    Checks the dimensions of output parameters of nn.deep._initialize_parameters().
     """
     layer_dims = [5, 4, 3, 2, 1]
     params = deep._initialize_parameters(layer_dims)
 
     for l in range(1, len(layer_dims)):
-        assert params[l][0].shape == (layer_dims[l], layer_dims[l - 1])
-        assert params[l][1].shape == (layer_dims[l], 1)
+        assert params['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1])
+        assert params['b' + str(l)].shape == (layer_dims[l], 1)
 
 
 def test_nn_deep_initialize_parameters_weight_nonzero():
     """
-    Tests that nn.shallow._initialize_parameters() does not set the weight matrices to zero
-    matrices.
+    Tests that nn.deep._initialize_parameters() does not set the weight matrices to zero matrices.
     """
     layer_dims = [5, 4, 3, 2, 1]
     params = deep._initialize_parameters(layer_dims)
 
     for l in range(1, len(layer_dims)):
-        assert np.count_nonzero(params[l][0]) > 0
+        assert np.count_nonzero(params['W' + str(l)]) > 0
 
 
 def test_nn_deep_initialize_parameters_bias_zero():
     """
-    Tests that nn.shallow._initialize_parameters() sets the bias vectors to zero vectors.
+    Tests that nn.deep._initialize_parameters() sets the bias vectors to zero vectors.
     """
     layer_dims = [5, 4, 3, 2, 1]
     params = deep._initialize_parameters(layer_dims)
 
     for l in range(1, len(layer_dims)):
-        assert np.count_nonzero(params[l][1]) == 0
+        assert np.count_nonzero(params['b' + str(l)]) == 0
