80841c41dada0b380dd62e97bf20935b43b45c29
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-04-02 14:04:44 +0900

    Added sigmoid_derivative() to activation.py

--- ryann/activation.py
+++ ryann/activation.py
@@ -21,3 +21,23 @@ def sigmoid(x):
         with sigmoid function applied to each element of the ndarray.
     """
     return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_derivative(x):
+    """
+    Applies the derivative of the sigmoid function to a number of to a NumPy ndarray.
+
+    Parameters
+    ----------
+
+    x : float or np.ndarray
+        A single number or a ndarray to apply the sigmoid' function.
+
+    Returns
+    -------
+    float or ndarray
+        If x was a single float, returns sigmoid'(x). If x was an np.ndarray, returns a np.ndarray
+        with sigmoid' function applied to each element of the ndarray.
+
+    """
+    return sigmoid(x) * (1 - sigmoid(x))
