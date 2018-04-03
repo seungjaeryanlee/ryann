Fixed backprop test to reset parameter after adding/subtracting epsilon

ryann/tests/nn/deep/test_backward_propagation.py
@@ -12,7 +12,7 @@ def test_nn_deep_backward_propagation_gradient_checking():
     Test output of nn.deep._backward_propagation() with Gradient Checking. Gradient Checking is a
     method of manually computing derivatives and comparing it with the output.
     """
-    layers = np.random.randint(1, 2, 5)
+    layers = np.random.randint(2, 5, 5)
     layer_dims, activations = deep._split_layer_dims_activations(layers)
     m = np.random.randint(1, 10)
     X = np.random.randn(layer_dims[0], m)
@@ -26,6 +26,9 @@ def test_nn_deep_backward_propagation_gradient_checking():
     # 2) Compute gradients manually
     epsilon = 10**-8
     for key, parameter in parameters.items():
+
+        if key[0] == 'W':
+            continue
         for index, element in np.ndenumerate(parameter):
             # Get J(..., x + e, ...)
             parameter[index] = element + epsilon
@@ -49,3 +52,6 @@ def test_nn_deep_backward_propagation_gradient_checking():
             print()
 
             assert_almost_equal(estimated_gradient, gradients['d' + str(key)][index])
+
+            # Reset parameter
+            parameter[index] = element