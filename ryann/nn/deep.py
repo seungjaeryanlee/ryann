8340cfbd36137ac93204c948a56b758943045c40
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-04-03 16:00:04 +0900

    Implemented deep.backprop with regularization

--- ryann/nn/deep.py
+++ ryann/nn/deep.py
@@ -282,6 +282,53 @@ def _backward_propagation(cache, Y_computed, Y, activations):
     return gradients
 
 
+def _backward_propagation_with_regularization(cache, Y_computed, Y, activations, lambd):
+    """
+    Runs backward propagation with L2 regularization on given parameters using cached values, X,
+    and Y.
+
+    Parameters
+    ----------
+    cache : dict
+        A dictionary with 3L+1 values, where L is the number of layers. Each layer l has Zl, the
+        result of the linear action, Al, the result of the nonlinear activation function, and Wl,
+        the weight matrix. A0 is added for convenience but is not used. The dictionary was filled
+        from forward propagation.
+    Y_computed : np.ndarray
+        The sigmoid output of the neural network with shape (n_y, m).
+    Y : np.ndarray
+        The matrix with correct labels with shape (n_y, m).
+    activations : list of str
+        A list of names of activation functions to use for each layer.
+    lambd : float
+        The regularization constant. The higher lambd is, the simpler the model becomes.
+
+    Returns
+    -------
+    gradients : dict
+        A dictionary of gradients with respect to given parameters. Each layer l has 3 gradients:
+        dAl, dWl, dbl.
+    """
+    gradients = {}
+    L = (len(cache) - 1) // 3
+    m = Y_computed.shape[1]
+
+    # Calculate gradient of last activation: Y_computed
+    dA = -(Y / Y_computed - (1 - Y) / (1 - Y_computed))
+
+    # from L to 1:
+    for l in reversed(range(1, L + 1)):
+        dZ = _activation_backward(dA, cache['Z' + str(l)], activations[l])
+        A_prev = cache['A' + str(l - 1)]
+        W = cache['W' + str(l)]
+        gradients['dW' + str(l)] = 1 / m * np.dot(dZ, A_prev.T) + lambd / m * W
+        gradients['db' + str(l)] = 1 / m * np.sum(dZ, axis=1, keepdims=True)
+        gradients['dA' + str(l - 1)] = np.dot(W.T, dZ)
+        dA = gradients['dA' + str(l - 1)] # Update dA for previous layer
+
+    return gradients
+
+
 def _activation_backward(dAl, Zl, activation_name):
     """
     Implement the backward propagation for the LINEAR->ACTIVATION layer.
