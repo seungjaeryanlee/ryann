94b0a1edb1fa5a839e0c8239e7156c4cb7e6c6a1
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-04-03 16:39:52 +0900

    Added seed to generate same example every time

--- examples/example_nn_deep_with_regularization.py
+++ examples/example_nn_deep_with_regularization.py
@@ -10,19 +10,20 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 from ryann.nn import deep
 
+np.random.seed(100)
 
 # 0) Synthesize Data
-m = 100
+m = 200
 X = np.random.randn(2, m)
 Y = np.zeros(shape=(1, m))
 for i, x in enumerate(X.T):
-    Y[0][i] = int(x[0] + 2 * x[1] - 1 < 0)
+    Y[0][i] = int(2 * x[0] + x[1] - 1 < 0)
     if np.random.choice(20) == 7 and abs(x[0] + 2 * x[1] - 1) > 1:
         Y[0][i] = 1 - Y[0][i]
 
 # 1) Train
 parameters, costs = deep.train(X, Y, layers=[2, 10, 10, 10, 10, 10, 10, 1],
-                               num_iter=2000, learning_rate=0.01, regularization=False)
+                               num_iter=2000, learning_rate=0.1, regularization=False)
 
 # 2) Compute and Plot Decision Boundary
 x1_pts = np.arange(-5, 5, 0.01)
@@ -36,9 +37,9 @@ plt.contourf(x1_grid, x2_grid, Z, cmap='coolwarm')
 plt.scatter(X[0, :], X[1, :], c=Y.reshape(m), cmap='coolwarm')
 plt.show()
 
-# 3) Train regularizaed model
+# 3) Train regularized model
 parameters, costs = deep.train(X, Y, layers=[2, 10, 10, 10, 10, 10, 10, 1],
-                               num_iter=2000, learning_rate=0.01, regularization=True, lambd=1)
+                               num_iter=2000, learning_rate=0.1, regularization=True, lambd=0.1)
 
 # 4) Compute and Plot Decision Boundary for regularized model
 x1_pts = np.arange(-5, 5, 0.01)
