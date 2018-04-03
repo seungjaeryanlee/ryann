6ee65f6eacc02e32fb768173428ca55410e6a21a
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-04-03 16:32:22 +0900

    Improved deep nn regularization example by comparison

--- examples/example_nn_deep_with_regularization.py
+++ examples/example_nn_deep_with_regularization.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This file showcases the ryann.nn.deep module with regularization enabled.
+This file showcases the ryann.nn.deep module.
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,24 +12,40 @@ from ryann.nn import deep
 
 
 # 0) Synthesize Data
-m = 200
+m = 100
 X = np.random.randn(2, m)
 Y = np.zeros(shape=(1, m))
-noise = 1
 for i, x in enumerate(X.T):
-    Y[0][i] = int(x[0]**2 + 2 * x[1] < 1 + noise * np.random.choice([0, 1]))
-# plt.scatter(X[0, :], X[1, :], c=Y.reshape(m), cmap='coolwarm')
+    Y[0][i] = int(x[0] + 2 * x[1] - 1 < 0)
+    if np.random.choice(20) == 7 and abs(x[0] + 2 * x[1] - 1) > 1:
+        Y[0][i] = 1 - Y[0][i]
 
-# 2) Train
-parameters, costs = deep.train(X, Y, layers=[2, 2, 2, 1], num_iter=10000, learning_rate=0.01,
-                               regularization=True, lambd=0.0001)
+# 1) Train
+parameters, costs = deep.train(X, Y, layers=[2, 10, 10, 10, 10, 10, 10, 1],
+                               num_iter=2000, learning_rate=0.01, regularization=False)
 
-# 3) Compute and Plot Decision Boundary
+# 2) Compute and Plot Decision Boundary
 x1_pts = np.arange(-5, 5, 0.01)
 x2_pts = np.arange(-5, 5, 0.01)
 x1_grid, x2_grid = np.meshgrid(x1_pts, x2_pts)
 X_grid = np.concatenate((x1_grid.reshape(1, -1), x2_grid.reshape(1, -1)))
-Z = deep.predict(parameters, ['relu', 'relu', 'relu', 'sigmoid'], X_grid)
+Z = deep.predict(parameters, ['relu'] * 7 + ['sigmoid'], X_grid)
+Z = Z.reshape((len(x1_pts), len(x2_pts)))
+
+plt.contourf(x1_grid, x2_grid, Z, cmap='coolwarm')
+plt.scatter(X[0, :], X[1, :], c=Y.reshape(m), cmap='coolwarm')
+plt.show()
+
+# 3) Train regularizaed model
+parameters, costs = deep.train(X, Y, layers=[2, 10, 10, 10, 10, 10, 10, 1],
+                               num_iter=2000, learning_rate=0.01, regularization=True, lambd=1)
+
+# 4) Compute and Plot Decision Boundary for regularized model
+x1_pts = np.arange(-5, 5, 0.01)
+x2_pts = np.arange(-5, 5, 0.01)
+x1_grid, x2_grid = np.meshgrid(x1_pts, x2_pts)
+X_grid = np.concatenate((x1_grid.reshape(1, -1), x2_grid.reshape(1, -1)))
+Z = deep.predict(parameters, ['relu'] * 7 + ['sigmoid'], X_grid)
 Z = Z.reshape((len(x1_pts), len(x2_pts)))
 
 plt.contourf(x1_grid, x2_grid, Z, cmap='coolwarm')
